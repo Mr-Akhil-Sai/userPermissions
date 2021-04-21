008279a733c3a3b052245ffd1763279bacfc130c
Mr.AK <akhilsaivaraganti@gmail.com>
2021-04-22 00:00:33 +0530

    third commit

--- userPermission.sh
+++ userPermission.sh
@@ -1,14 +1,32 @@
 
 creatingUser(){
-    sudo adduser --forece-badname $1
+    sudo adduser --force-badname $1
+    echo " new User $1 created "
 }
 
+creatingFolder(){
+    mkdir $1 
+    cd $1
+    echo "new folder named $1 is created"
+}
 
+creatingFile(){
+    touch $1.txt
+    echo "new file named $1 is created"
+    cd ..
+}
 echo "WELCOME DUDE"
 
-read -p "Enter the user name: " userName1
-creatingUser userName1
+read -p "Enter no of users: " noOfUsers
+i=0
+for i in noOfUsers
+do 
+    read -p "Enter the user name: " userNamei
+    creatingUser $userNamei
 
-read -p "Enter the folder name: " user1Folder
+    read -p "Enter the folder name: " useriFolder
+    creatingFolder $useriFolder
 
-read -p "Enter the file name: " user1File
+    read -p "Enter the file name: " useriFile
+    creatingFile $useriFile
+done
